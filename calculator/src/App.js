b63ff74d94e051708f6088f68611299dcc6217d5
stefnotadevyet <srjack91@gmail.com>
2021-08-18 20:57:22 -0400

    wrote layout and added styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,7 +5,42 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          test
+          <input type="text" id="userInp" placeholder="0" disabled/>
+          <input type="text" id="calcOut" placeholder="0" disabled/>
+          <table>
+           <tbody>
+              <tr>
+                <td onclick="">(</td>
+                <td onclick="">)</td>
+                <td onclick="">&lt;-</td>
+                <td onclick="" className="vari">+</td>
+              </tr>
+              <tr>
+                <td onclick="">7</td>
+                <td onclick="">8</td>
+                <td onclick="">9</td>
+                <td onclick="" className="vari">-</td>
+              </tr>
+              <tr>
+                <td onclick="">4</td>
+                <td onclick="">5</td>
+                <td onclick="">6</td>
+                <td onclick="" className="vari">*</td>
+              </tr>
+              <tr>
+                <td onclick="">1</td>
+                <td onclick="">2</td>
+                <td onclick="">3</td>
+                <td onclick="" className="vari">/</td>
+              </tr>
+              <tr>
+                <td onclick="">C</td>
+                <td onclick="">0</td>
+                <td onclick="">.</td>
+                <td onclick="" className="eqlBtn">=</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </header>
     </div>
